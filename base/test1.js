f86e1cf5f4246ed710a869a9a1072574a6544a13
vivekpundir17 <156446532+vivekpundir17@users.noreply.github.com>
2024-01-12 18:19:49 +0000

    done

--- base/test1.js
+++ base/test1.js
@@ -1,1 +1,27 @@
-console.log("vivek")
+
+const a="1212"
+let b = "pundheer"
+var c = "abhi"
+name = "viv"
+
+name = "vivek"
+b="Pundir"
+c="Abhishek"
+
+
+console.table([a,name,b,c,b])
+
+/* DATA TYPES
+int
+bigint
+string
+number
+null = standalone value
+boolean
+undefined = typeof undefined
+symbol = unique 
+object = typeof null
+*/
+
+console.log(typeof undeifned)
+console.log(typeof null)
---
+++ base/test2.js
@@ -0,0 +1,14 @@
+let score = undefined
+let value = Number(score)
+console.log(typeof score)
+console.log(typeof value)
+console.log(value)
+// NaN => Not a number
+let loggedin = "ds"
+let isloggedin=Boolean(loggedin)
+console.log(isloggedin);
+
+let num = 45
+let isnum = String(num)
+console.log(isnum)
+console.log(typeof isnum)
