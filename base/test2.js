e99d20a46901e857e63857845d67bbfcbda662d4
vivekpundir17 <156446532+vivekpundir17@users.noreply.github.com>
2024-01-13 13:29:05 +0000

    done

--- base/test2.js
+++ base/test2.js
@@ -12,3 +12,13 @@ let num = 45
 let isnum = String(num)
 console.log(isnum)
 console.log(typeof isnum)
+
+let str1="hello"
+let str2 = " vivek"
+let str3 = str1+str2
+console.log(str3)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+3)
+console.log(4+2+"3")
---
+++ base/test3.js
@@ -0,0 +1,7 @@
+//PRIMITIVE DATATYPES
+//  null, undefined ,int ,bigint, boolean ,symbol, string 
+
+console.log(2>="1")
+
+//NON - PRIMITIVE TYPES
+// array, objects, function
